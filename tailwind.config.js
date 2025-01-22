cb8ea3f0dabd5f43de37bcd279deba097d138ea1
noemi <noemibonilla95@hotmail.com>
2025-01-22 17:47:24 +0100

    adding theme and personal colors's proyect

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,jsx,js}"],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
+    colors: {
+      "cool-gray": "#cbcbcb",
+      "black-slate": "#2b2b2b",
+      "white": "#f2f2f2",
+      "cool-green": "#174d38",
+      "cool-red": "#4d1717",
+      "red-light": "#988686",
+    },
+    fontFamily:{
+      "sans": "DM Sans"
+    },
     extend: {},
   },
   plugins: [],
